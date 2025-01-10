617f354d800e81cf4bfc5bd9c2fc364e6ee0f06f
Otoniel Muñoz <rmunoz1803@alumno.ipn.mx>
2025-01-10 12:10:46 -0600

    Adding conversion.java

--- src/main/java/currency/converter/Conversion.java
+++ src/main/java/currency/converter/Conversion.java
@@ -1,4 +1,17 @@
 package currency.converter;
 
 public class Conversion {
+    private String monedaOrigen;
+    private String monedaDestino;
+    private double cantidadOrigen;
+    private double cantidadDestino;
+    private double tasaCambio;
+
+    public Conversion(String monedaOrigen, String monedaDestino, double cantidadOrigen, double cantidadDestino, double tasaCambio) {
+        this.monedaOrigen = monedaOrigen;
+        this.monedaDestino = monedaDestino;
+        this.cantidadOrigen = cantidadOrigen;
+        this.cantidadDestino = cantidadDestino;
+        this.tasaCambio = tasaCambio;
+    }
 }
